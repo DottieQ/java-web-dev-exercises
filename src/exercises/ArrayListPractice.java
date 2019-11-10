ec83fcc20f126eab901586dc6d6cb2a09bcf3b42
Dottie <dottiequick@gmail.com>
2019-11-10 17:12:54 -0600

    finished exercises

--- src/exercises/ArrayListPractice.java
+++ src/exercises/ArrayListPractice.java
@@ -1,9 +1,44 @@
 package exercises;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListPractice {
     public static void main(String[] args) {
+
+        ArrayList<Integer> numList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            numList.add(i);
+        }
+
+        int numTotal = 0;
+
+        for (int i = 0; i < numList.size(); i++)
+            if (numList.get(i) % 2 == 0) {
+                numTotal += numList.get(i);
+            }
+        System.out.println(numTotal);
+
+            ArrayList<String> fiveCharWords = new ArrayList<>();
+            fiveCharWords.add("house");
+            fiveCharWords.add("at");
+            fiveCharWords.add("harry");
+            fiveCharWords.add("henderson");
+            fiveCharWords.add("sweet");
+            fiveCharWords.add("ants");
+
+            Scanner input = new Scanner(System.in);
+           // int selectedLength;
+            System.out.println("Enter a word length:");
+            int selectedLength = input.nextInt();
+
+
+            for (String word : fiveCharWords) {
+                if (word.length() == selectedLength) {
+                    System.out.println(word);
+                }
+            }
 
     }
 }
--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -1,7 +1,36 @@
 package exercises;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class HashMapPractice {
     public static void main(String[] args) {
+        HashMap<Integer, String> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        String newStudent;
+
+        System.out.println("Enter student names (press ENTER twice to finish)");
+
+        do {
+            System.out.print("Student name: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("ID: ");
+                Integer newId = input.nextInt();
+                students.put(newId, newStudent);
+
+                input.nextLine();
+            }
+        } while(!newStudent.equals(""));
+
+        System.out.println("Student Roster: ");
+
+        for (Map.Entry<Integer, String> student : students.entrySet()) {
+            System.out.println("Name: " + student.getValue());
+            System.out.println("ID: " + student.getKey());
+        }
 
     }
 }
